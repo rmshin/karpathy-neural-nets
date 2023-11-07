a206345c51996b898f18b00d95618732dd385c56
rmshin <50217797+rmshin@users.noreply.github.com>
2023-11-07 15:59:53 -0500

    py format

--- makemore/makemore_bg.py
+++ makemore/makemore_bg.py
@@ -1,40 +1,40 @@
 # bigram model implementation
 import torch, string
 
-words = open('makemore/names.txt', 'r').read().splitlines()
+words = open("makemore/names.txt", "r").read().splitlines()
 
 ########## CLASSICAL STATISTICS MODEL ##########
 
 chars = list(string.ascii_lowercase)
-ctoi = {c:i+1 for (i, c) in enumerate(chars)}
-ctoi["."] = 0 # special char to represent start and end of word
-itoc = {i:c for (c, i) in ctoi.items()}
+ctoi = {c: i + 1 for (i, c) in enumerate(chars)}
+ctoi["."] = 0  # special char to represent start and end of word
+itoc = {i: c for (c, i) in ctoi.items()}
 
 # init with 1's to avoid 0 probability of zero frequency bigrams
 N = torch.full((27, 27), 1, dtype=torch.int32)
 for word in words:
-  word = itoc[0] + word + itoc[0]
-  for (c1, c2) in zip(word, word[1:]):
-    i1 = ctoi[c1]
-    i2 = ctoi[c2]
-    N[i1, i2] += 1
+    word = itoc[0] + word + itoc[0]
+    for c1, c2 in zip(word, word[1:]):
+        i1 = ctoi[c1]
+        i2 = ctoi[c2]
+        N[i1, i2] += 1
 
 # create probability distributions for every bigram combination
 P = N.float()
-P /= P.sum(1, keepdim=True) # ensure sum of row rather than column
+P /= P.sum(1, keepdim=True)  # ensure sum of row rather than column
 
 # sample model
 g = torch.Generator().manual_seed(2147483647)
 for i in range(20):
-  out = []
-  ix = 0
-  while True:
-    p = P[ix]
-    ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
-    if ix == 0:
-      break
-    out.append(itoc[ix])
-  print("".join(out))
+    out = []
+    ix = 0
+    while True:
+        p = P[ix]
+        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+        if ix == 0:
+            break
+        out.append(itoc[ix])
+    print("".join(out))
 
 
 ########## NEURAL NETWORK MODEL ##########
@@ -42,12 +42,12 @@ for i in range(20):
 # xs = inputs (first bigram letter index), ys = labels (associated second bigram letter index)
 xs, ys = [], []
 for word in words:
-  word = itoc[0] + word + itoc[0]
-  for (c1, c2) in zip(word, word[1:]):
-    i1 = ctoi[c1]
-    i2 = ctoi[c2]
-    xs.append(i1)
-    ys.append(i2)
+    word = itoc[0] + word + itoc[0]
+    for c1, c2 in zip(word, word[1:]):
+        i1 = ctoi[c1]
+        i2 = ctoi[c2]
+        xs.append(i1)
+        ys.append(i2)
 ys = torch.tensor(ys)
 num = len(xs)
 
@@ -57,41 +57,48 @@ W = torch.randn((27, 27), generator=g, requires_grad=True)
 
 # gradient descent
 for k in range(20):
-  ### forward pass
-  # each ix value in xs represents character index, W[ix] = "probability distribution" of itoc[ix]
-  logits = torch.stack([W[ix] for ix in xs]) # builds probability distribution matrix
-  counts = logits.exp()
-  P_NN = counts / counts.sum(1, keepdim=True)
-  loss = -P_NN[torch.arange(num), ys].log().mean() + 0.01*(W**2).mean() # add regularisation
+    ### forward pass
+    # each ix value in xs represents character index, W[ix] = "probability distribution" of itoc[ix]
+    logits = torch.stack([W[ix] for ix in xs])  # builds probability distribution matrix
+    counts = logits.exp()
+    P_NN = counts / counts.sum(1, keepdim=True)
+    loss = (
+        -P_NN[torch.arange(num), ys].log().mean() + 0.01 * (W**2).mean()
+    )  # add regularisation
 
-  ### backward pass
-  W.grad = None # set to zero the gradient
-  loss.backward()
+    ### backward pass
+    W.grad = None  # set to zero the gradient
+    loss.backward()
 
-  ### update
-  W.data += -50 * W.grad
+    ### update
+    W.data += -50 * W.grad
 
 # calculate post-training loss
 logits = torch.stack([W[ix] for ix in xs])
 counts = logits.exp()
 P_NN = counts / counts.sum(1, keepdim=True)
 loss = -P_NN[torch.arange(num), ys].log().mean()
-print(loss) # tensor(2.5727, grad_fn=<NegBackward0>)
+print(loss)  # tensor(2.5727, grad_fn=<NegBackward0>)
 
 # sample model
 g = torch.Generator().manual_seed(2147483647)
+
+
 def generate_words(num_words, g):
-  for _ in range(num_words):
-    out = []
-    ix = 0
-    while True:
-      logits = torch.stack([W[ix]])
-      counts = logits.exp()
-      p = counts / counts.sum(1, keepdim=True)
-      ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
-      out.append(itoc[ix])
-      if ix == 0:
-        break
-    print("".join(out))
+    for _ in range(num_words):
+        out = []
+        ix = 0
+        while True:
+            logits = torch.stack([W[ix]])
+            counts = logits.exp()
+            p = counts / counts.sum(1, keepdim=True)
+            ix = torch.multinomial(
+                p, num_samples=1, replacement=True, generator=g
+            ).item()
+            out.append(itoc[ix])
+            if ix == 0:
+                break
+        print("".join(out))
+
 
 generate_words(20, g)
--- makemore/makemore_mlp.py
+++ makemore/makemore_mlp.py
@@ -2,131 +2,149 @@
 import torch, random
 import torch.nn.functional as F
 
-words = open('makemore/names.txt', 'r').read().splitlines()
+words = open("makemore/names.txt", "r").read().splitlines()
 chars = sorted(list(set("".join(words))))
-stoi = {c: i+1 for i, c in enumerate(chars)}
+stoi = {c: i + 1 for i, c in enumerate(chars)}
 stoi["."] = 0
 itos = {i: c for c, i in stoi.items()}
 
 # generate examples from dataset
 block_size = 3
+
+
 def build_dataset(words):
-  xs, ys = [], []
-  for w in words:
-    context = [0] * block_size
-    for c in (w + "."):
-      xs.append(context)
-      ys.append(stoi[c])
-      context = context[1:] + [stoi[c]]
-  X = torch.tensor(xs) # torch.Size([228146, 3])
-  Y = torch.tensor(ys) # torch.Size([228146])
-  return X, Y
+    xs, ys = [], []
+    for w in words:
+        context = [0] * block_size
+        for c in w + ".":
+            xs.append(context)
+            ys.append(stoi[c])
+            context = context[1:] + [stoi[c]]
+    X = torch.tensor(xs)  # torch.Size([228146, 3])
+    Y = torch.tensor(ys)  # torch.Size([228146])
+    return X, Y
+
 
 # split dataset into training, dev/validation, and test (80:10:10)
 random.seed(42)
 random.shuffle(words)
-n1 = int(0.8*len(words))
-n2 = int(0.9*len(words))
+n1 = int(0.8 * len(words))
+n2 = int(0.9 * len(words))
 Xtr, Ytr = build_dataset(words[:n1])
 Xdev, Ydev = build_dataset(words[n1:n2])
 Xte, Yte = build_dataset(words[n2:])
 
 # create embeddings table (first layer)
-embed_dim = 5 # 2D vector embeddings
-C = torch.randn((len(chars) + 1, embed_dim)) # torch.Size([27, 2])
+embed_dim = 5  # 2D vector embeddings
+C = torch.randn((len(chars) + 1, embed_dim))  # torch.Size([27, 2])
 
 # create weights + biases (hidden layer)
-num_neurons = 200 # hyperparameter for tuning
-W1 = torch.randn((block_size * embed_dim, num_neurons)) * (5 / 3) / ((block_size * embed_dim)**0.5) # Kaiming initialisation, torch.Size([6, 100])
+num_neurons = 200  # hyperparameter for tuning
+W1 = (
+    torch.randn((block_size * embed_dim, num_neurons))
+    * (5 / 3)
+    / ((block_size * embed_dim) ** 0.5)
+)  # Kaiming initialisation, torch.Size([6, 100])
 # b1 = torch.randn(num_neurons, requires_grad=True) * 0.01 # torch.Size([100]) - not needed with batch normalisation
 
 # create weights + biases for final probability distribution tensor (final layer)
-W2 = torch.randn((num_neurons, len(chars) + 1)) * 0.01 # torch.Size([100, 27])
-b2 = torch.zeros(len(chars) + 1) # torch.Size([27])
+W2 = torch.randn((num_neurons, len(chars) + 1)) * 0.01  # torch.Size([100, 27])
+b2 = torch.zeros(len(chars) + 1)  # torch.Size([27])
 
 # for batch normalisation
 bngain = torch.ones((1, num_neurons))
 bnbias = torch.zeros((1, num_neurons))
 # estimated running bn mean & std
-bnmean_running = torch.zeros((1, num_neurons)) # initialise zero mean due to Kaiming normal init
-bnstd_running = torch.ones((1, num_neurons)) # initialise to unit std due to Kaiming normal init
+bnmean_running = torch.zeros(
+    (1, num_neurons)
+)  # initialise zero mean due to Kaiming normal init
+bnstd_running = torch.ones(
+    (1, num_neurons)
+)  # initialise to unit std due to Kaiming normal init
+
 
 def descend_gradient(X, Y, C, W1, W2, b2, bngain, bnbias, i):
-  parameters = [C, W1, W2, b2, bngain, bnbias]
-  # mini batching for faster training
-  ix = torch.randint(0, X.shape[0], (32,)) # torch.Size([32])
+    parameters = [C, W1, W2, b2, bngain, bnbias]
+    # mini batching for faster training
+    ix = torch.randint(0, X.shape[0], (32,))  # torch.Size([32])
 
-  ### forward pass
-  emb = C[X[ix]] # get embeddings only for batch, torch.Size([32, 3, 2])
-  h_preact = emb.view(emb.shape[0], -1) @ W1
-  bnmean_i = h_preact.mean(0, keepdim=True)
-  bnstd_i = h_preact.std(0, keepdim=True)
-  h_preact = bngain * (h_preact - bnmean_i) / bnstd_i + bnbias # batch normalisation
+    ### forward pass
+    emb = C[X[ix]]  # get embeddings only for batch, torch.Size([32, 3, 2])
+    h_preact = emb.view(emb.shape[0], -1) @ W1
+    bnmean_i = h_preact.mean(0, keepdim=True)
+    bnstd_i = h_preact.std(0, keepdim=True)
+    h_preact = bngain * (h_preact - bnmean_i) / bnstd_i + bnbias  # batch normalisation
 
-  with torch.no_grad():
-    global bnmean_running
-    global bnstd_running
-    bnmean_running = 0.999 * bnmean_running + 0.001 * bnmean_i
-    bnstd_running = 0.999 * bnstd_running + 0.001 * bnstd_i
+    with torch.no_grad():
+        global bnmean_running
+        global bnstd_running
+        bnmean_running = 0.999 * bnmean_running + 0.001 * bnmean_i
+        bnstd_running = 0.999 * bnstd_running + 0.001 * bnstd_i
 
-  h = torch.tanh(h_preact)
-  logits = h @ W2 + b2 # torch.Size([32, 27])
-  loss = F.cross_entropy(logits, Y[ix])
+    h = torch.tanh(h_preact)
+    logits = h @ W2 + b2  # torch.Size([32, 27])
+    loss = F.cross_entropy(logits, Y[ix])
 
-  ### backward pass
-  for p in parameters:
-    p.grad = None
-  loss.backward()
+    ### backward pass
+    for p in parameters:
+        p.grad = None
+    loss.backward()
 
-  ### update
-  lr = 0.1 if i < 200000 else 0.01 # basic learning rate decay
-  for p in parameters:
-    p.data += -lr * p.grad
+    ### update
+    lr = 0.1 if i < 200000 else 0.01  # basic learning rate decay
+    for p in parameters:
+        p.data += -lr * p.grad
+
 
 # train model
 for p in [C, W1, W2, b2, bngain, bnbias]:
-  p.requires_grad = True
+    p.requires_grad = True
 
 for i in range(300000):
-  descend_gradient(Xtr, Ytr, C, W1, W2, b2, bngain, bnbias, i)
+    descend_gradient(Xtr, Ytr, C, W1, W2, b2, bngain, bnbias, i)
 
 # compare loss for training & dev
 with torch.no_grad():
-  emb_tr = C[Xtr] # torch.Size([32, 3, 2])
-  h_tr_preact = emb_tr.view(emb_tr.shape[0], -1) @ W1 # torch.Size([32, 100])
-  h_tr_preact = bngain * (h_tr_preact - bnmean_running) / bnstd_running + bnbias
-  h_tr = torch.tanh(h_tr_preact) # torch.Size([32, 100])
-  logits_tr = h_tr @ W2 + b2 # torch.Size([32, 27])
-  loss_tr = F.cross_entropy(logits_tr, Ytr)
-  print(loss_tr)
+    emb_tr = C[Xtr]  # torch.Size([32, 3, 2])
+    h_tr_preact = emb_tr.view(emb_tr.shape[0], -1) @ W1  # torch.Size([32, 100])
+    h_tr_preact = bngain * (h_tr_preact - bnmean_running) / bnstd_running + bnbias
+    h_tr = torch.tanh(h_tr_preact)  # torch.Size([32, 100])
+    logits_tr = h_tr @ W2 + b2  # torch.Size([32, 27])
+    loss_tr = F.cross_entropy(logits_tr, Ytr)
+    print(loss_tr)
 
-  emb_dev = C[Xdev]
-  h_dev_preact = emb_dev.view(emb_dev.shape[0], -1) @ W1
-  h_dev_preact = bngain * (h_dev_preact - bnmean_running) / bnstd_running + bnbias
-  h_dev = torch.tanh(h_dev_preact)
-  logits_dev = h_dev @ W2 + b2
-  loss_dev = F.cross_entropy(logits_dev, Ydev)
-  print(loss_dev)
+    emb_dev = C[Xdev]
+    h_dev_preact = emb_dev.view(emb_dev.shape[0], -1) @ W1
+    h_dev_preact = bngain * (h_dev_preact - bnmean_running) / bnstd_running + bnbias
+    h_dev = torch.tanh(h_dev_preact)
+    logits_dev = h_dev @ W2 + b2
+    loss_dev = F.cross_entropy(logits_dev, Ydev)
+    print(loss_dev)
 
 # sample from model
 g = torch.Generator().manual_seed(2147483647)
+
+
 def generate_words(num_words, g):
-  for _ in range(num_words):
-    out = []
-    context = [0] * block_size
-    ix = 0
-    while True:
-      emb = C[torch.tensor([context])] # torch.Size([1, 3, 2])
-      h_preact = emb.view(1, -1) @ W1 # torch.Size([1, 100])
-      h_preact = bngain * (h_preact - bnmean_running) / bnstd_running + bnbias
-      h = torch.tanh(h_preact) # torch.Size([1, 100])
-      logits = h @ W2 + b2 # torch.Size([1, 27])
-      probs = F.softmax(logits, dim=1)
-      ix = torch.multinomial(probs, num_samples=1, replacement=True, generator=g).item()
-      out.append(itos[ix])
-      if (ix == 0):
-        break
-      context = context[1:] + [ix]
-    print("".join(out))
+    for _ in range(num_words):
+        out = []
+        context = [0] * block_size
+        ix = 0
+        while True:
+            emb = C[torch.tensor([context])]  # torch.Size([1, 3, 2])
+            h_preact = emb.view(1, -1) @ W1  # torch.Size([1, 100])
+            h_preact = bngain * (h_preact - bnmean_running) / bnstd_running + bnbias
+            h = torch.tanh(h_preact)  # torch.Size([1, 100])
+            logits = h @ W2 + b2  # torch.Size([1, 27])
+            probs = F.softmax(logits, dim=1)
+            ix = torch.multinomial(
+                probs, num_samples=1, replacement=True, generator=g
+            ).item()
+            out.append(itos[ix])
+            if ix == 0:
+                break
+            context = context[1:] + [ix]
+        print("".join(out))
+
 
 generate_words(20, g)
