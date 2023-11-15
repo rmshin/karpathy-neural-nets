12ee91bf7c398e2f631e99fea14832eeabb435a6
rmshin <50217797+rmshin@users.noreply.github.com>
2023-11-14 19:17:13 -0500

    py format

--- nanogpt/train.py
+++ nanogpt/train.py
@@ -1,29 +1,31 @@
-import torch, random
-import torch.nn as nn
+import torch
 
-with open('nanogpt/input.txt', 'r', encoding='utf-8') as f:
+with open("nanogpt/input.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 chars = sorted(list(set(text)))
-stoi = { c: i for i, c in enumerate(chars)}
-itos = { i: c for c, i in stoi.items()}
-encode = lambda s: [stoi[c] for c in s] # convert string to list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # convert list of integers to string
+stoi = {c: i for i, c in enumerate(chars)}
+itos = {i: c for c, i in stoi.items()}
+encode = lambda s: [stoi[c] for c in s]  # convert string to list of integers
+decode = lambda l: "".join([itos[i] for i in l])  # convert list of integers to string
 
 batch_size = 4
 block_size = 8
 
 data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data))
+n = int(0.9 * len(data))
 train_data = data[:n]
 val_data = data[n:]
 
 torch.manual_seed(1337)
+
+
 def get_batch(split):
-  data = train_data if split == 'train' else val_data
-  ix = torch.randint(len(data) - block_size, (batch_size,))
-  x = torch.stack([data[i:i+block_size] for i in ix])
-  y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-  return x, y
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i : i + block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
+    return x, y
+
 
 Xb, Yb = get_batch("train")
